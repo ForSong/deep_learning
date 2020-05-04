6a200ac432e1c7777aece618d690e31f07952af4
zhizhuo song <forsongzz@hotmail.com>
2020-05-04 10:46:51 +0100

    深度学习实现：线性回归
    模型的保存和读取
    命令行参数的获取

--- 线性回归.py
+++ 线性回归.py
@@ -4,7 +4,6 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-
 # 训练参数，在Variable变量类型中，有一个参数trainable如果设置为True那么就会不断的优化，如果是False
 # 就不会变化，相当于constant。
 
@@ -15,6 +14,19 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # 如何解决梯度爆炸？1. 调整学习率；2.
 
 # 添加权重参数，损失值等在tensorboard观察的情况步骤：1. 收集变量 2. 合并变量写入时间文件
+
+# 定义命令行参数
+# 1. 首先定义有哪些参数需要在命令行运行时指定
+# 2. 程序当中获取定义命令行参数
+
+# 定义需要指定的参数
+# 对于这个方法的参数，第一个参数是方法的名字，第二个参数是方法的默认值，第三个参数是方法的说明
+tf.app.flags.DEFINE_integer("max_step", 100, "模型训练的步数")
+tf.app.flags.DEFINE_string("model_dir", "", "模型文件的加载路径")
+
+# 定义获取命令行参数的名字
+FLAGS = tf.app.flags.FLAGS
+
 
 def my_regression():
     """
@@ -52,6 +64,9 @@ def my_regression():
     # 2. 合并变量并写入时间文件,返回一个op
     merged = tf.summary.merge_all()
 
+    # 定义一个保存模型的实例
+    saver = tf.train.Saver()
+
     # 通过会话运行程序
     with tf.Session() as sess:
         # 初始化变量
@@ -60,14 +75,24 @@ def my_regression():
         print("随机初始化的参数权重为： %f, 偏置为: %f" % (weight.eval(), bias.eval()))
         # 建立时间文件
         fileWriter = tf.summary.FileWriter("./summary/test", graph=sess.graph)
-
+        # 加载模型，覆盖模型当中随机定义的参数，从上次训练的参数结果开始
+        # 首先判断是否有已经输出的模型文件
+        if os.path.exists('./model/regression'):
+            saver.restore(sess, './model/regression')
         # 循环训练运行优化
-        for i in range(300):
+        for i in range(FLAGS.max_step):
             sess.run(train_op)
             # 运行合并的tensor
             summary = sess.run(merged)
             fileWriter.add_summary(summary, i)
             print("参数权重为：%f，偏置为：%f" % (weight.eval(), bias.eval()))
+            # 如果循环次数比较多的时候可以使用判断语句来保存模型
+            # 比如每100次运算保存一次模型
+        # 如果运算次数比较少的情况下，可以直接在模型运算完之后保存
+        # 注意要首先定义op，记住op永远是在session之前定义的
+        # checkpoint 文件是变量名称
+        # 第二个是保存的内容
+        saver.save(sess, FLAGS.model_dir)
 
 
 if __name__ == '__main__':
